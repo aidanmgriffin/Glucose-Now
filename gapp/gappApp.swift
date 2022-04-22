20a742f19ed08ffe45633230df81c2e7d09afa02
maudan567 <maudan567@gmail.com>
2022-04-22 14:52:02 -0700

    Initial Commit

---
+++ gapp/gappApp.swift
@@ -0,0 +1,17 @@
+//
+//  gappApp.swift
+//  gapp
+//
+//  Created by Aidan Griffin on 4/22/22.
+//
+
+import SwiftUI
+
+@main
+struct gappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
