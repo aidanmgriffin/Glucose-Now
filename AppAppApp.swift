4d78c5338f0900bb2211d08a90bc7caea7899b44
maudan567 <maudan567@gmail.com>
2022-04-22 14:18:04 -0700

    app

---
+++ AppAppApp.swift
@@ -0,0 +1,18 @@
+//
+//  AppAppApp.swift
+//  AppApp
+//
+//  Created by Aidan Griffin on 3/18/22.
+//
+
+
+import SwiftUI
+
+@main
+struct AppAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
